70576a2fd0549431d84bbc110fba9b1c6fb92136
akosborn <andrewosborn93@gmail.com>
2020-05-16 19:22:23 -0400

    Payment schedule table

--- src/calculator/MediaGallery.tsx
+++ src/calculator/MediaGallery.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {getNumberOfPayments, getPayoffAmount} from "./FinanceUtils";
+import {Table} from "semantic-ui-react";
 
 interface Props {
    balance: number;
@@ -15,18 +16,28 @@ function MediaGallery(props: Props) {
             <b><p>Months</p></b>
             <p>{numPayments}</p>
          </div>
-         {[...Array(Math.floor(numPayments) || 0)].map((_, i) =>
-            <div style={{ display: "flex" }}>
-               <div style={{ marginRight: 10 }}>
-                  <b><p>Interest at {i + 1} months</p></b>
-                  <p>{(getPayoffAmount(props.balance, props.rate, i + 1, props.payment) * props.rate).toFixed(2)}</p>
-               </div>
-               <div>
-                  <b><p>% of Payment to Interest at {i + 1} months</p></b>
-                  <p>{(((getPayoffAmount(props.balance, props.rate, i + 1, props.payment) * props.rate) / props.payment) * 100).toFixed()}</p>
-               </div>
-            </div>
-         )}
+         <Table celled>
+            <Table.Header>
+               <Table.Row>
+                  <Table.HeaderCell>Payment</Table.HeaderCell>
+                  <Table.HeaderCell>Balance</Table.HeaderCell>
+                  <Table.HeaderCell>Interest</Table.HeaderCell>
+                  <Table.HeaderCell>Interest % of Payment</Table.HeaderCell>
+               </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+               {[...Array(Math.floor(numPayments) || 0)].map((_, i) =>
+                  <Table.Row>
+                     <Table.Cell>{i + 1}</Table.Cell>
+                     <Table.Cell>{getPayoffAmount(props.balance, props.rate, i + 1, props.payment)}</Table.Cell>
+                     <Table.Cell>{(getPayoffAmount(props.balance, props.rate, i + 1, props.payment) * props.rate).toFixed(2)}</Table.Cell>
+                     <Table.Cell>{(((getPayoffAmount(props.balance, props.rate, i + 1, props.payment) * props.rate) / props.payment) * 100).toFixed()}</Table.Cell>
+                  </Table.Row>
+               )}
+            </Table.Body>
+         </Table>
+
          <div>
             <b><p>Final Payment</p></b>
             <p>{getPayoffAmount(props.balance, props.rate, Math.floor(numPayments), props.payment)}</p>
